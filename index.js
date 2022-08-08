4f9cefe600135ffe838b3288de0a94e63917a1a8
Anglebert-Dev <anglebertsh@gmail.com>
2022-08-08 10:58:18 +0200

    fixed cors error and added swagger documentation

--- index.js
+++ index.js
@@ -22,15 +22,19 @@ const Order = require("./routes/Order");
 const app = express();
 app.use(express.json());
 
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
 app.use("/api/user", User);
 app.use("/api/product", Product);
 app.use("/api/cart" , Cart);
 app.use("/api/order" , Order);
 
 // configure swagger
-app.use("/api-docs", 
-swaggerUi.serve, 
-swaggerUi.setup(swaggerDocument));
+app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 // SETTIMG PORTS
